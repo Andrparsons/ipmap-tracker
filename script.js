98fe7679afe6bb593ec184e79c0b1a22336b402b
Andre Parsons <andrparsons@gmail.com>
2021-01-29 07:42:18 -0500

    added leaflet to draw map

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // pull IP geodata
 
-// const ipEndpoint =
-//   "https://geo.ipify.org/api/v1?apiKey=at_jdQRUlqInO60YbkS1bSOpzsr4OC1t";
+const ipEndpoint =
+  "https://geo.ipify.org/api/v1?apiKey=at_jdQRUlqInO60YbkS1bSOpzsr4OC1t";
 const userIp = document.querySelector(".ip");
 const userLocation = document.querySelector(".location");
 const userTimezone = document.querySelector(".time");
 const userIsp = document.querySelector(".isp");
+let mapLat, mapLong;
 
 async function displayIPData() {
   const response = await fetch(ipEndpoint);
@@ -15,6 +16,16 @@ async function displayIPData() {
   userLocation.textContent = `${data.location.city}, ${data.location.region}`;
   userTimezone.textContent = data.location.timezone;
   userIsp.textContent = data.isp;
+  mapLat = data.location.lat;
+  mapLong = data.location.lng;
+
+  // map functions
+  const ipMap = L.map("map-component").setView([mapLat, mapLong], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  }).addTo(ipMap);
 }
 
 function handleError(err) {
